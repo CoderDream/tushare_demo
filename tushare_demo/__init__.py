7ba64229d3e365fb9a7250c6b459468c2ca836db
CoderDream <coderdream@gmail.com>
2017-10-20 16:30:14 +0800

    first version

---
+++ tushare_demo/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python  
+# coding: UTF-8  
+  
+"""This script parse stock info"""  
+  
+import tushare as ts  
+  
+def get_all_price(STOCK):  
+    '''''process all stock'''  
+    df = ts.get_realtime_quotes(STOCK)  
+    print df  
+  
+if __name__ == '__main__':  
+    STOCK = ['600219',       ##南山铝业  
+             '000002',       ##万  科Ａ  
+             '000623',       ##吉林敖东  
+             '000725',       ##京东方Ａ  
+             '600036',       ##招商银行  
+             '601166',       ##兴业银行  
+             '600298',       ##安琪酵母  
+             '600881',       ##亚泰集团  
+             '002582',       ##好想你  
+             '600750',       ##江中药业  
+             '601088',       ##中国神华  
+             '000338',       ##潍柴动力  
+             '000895',       ##双汇发展  
+             '000792']       ##盐湖股份  
+  
+    get_all_price(STOCK)  
